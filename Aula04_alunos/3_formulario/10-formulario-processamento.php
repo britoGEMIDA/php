dcb0602bd755638e9c998b3b4afbe740cb600f22
britoGEMIDA <febrito13santos@gmail.com>
2023-10-05 11:40:45 -0300

    .

--- Aula04_alunos/3_formulario/10-formulario-processamento.php
+++ Aula04_alunos/3_formulario/10-formulario-processamento.php
@@ -13,9 +13,25 @@
     <hr>
 
     <!-- ________________________________________________________________________________ -->
- 
+ <?php
+ // Detectar quando o formulário é acionado
+ if (   isset($_POST['enviar'])) {
 
-    <!-- 1ª Digitação (aqui) -->
+    // Validando se os campos foram preenchidos
+    if(empty($_POST['nome']) || empty($_POST['email'])) {
+?>
+    <!-- Se os campos vazios, solicita ao usuário o preenchimento -->
+    <p style="color:red">Preencha os campos</p>
+    <p><a href=""><link href="10-formulario-processamento.php">Voltar</a></p>
+<?php
+    } else {
+        // Usa a função de filtro para maior segurança
+        $nome = filter_input(INPUT_POST, "nome", FILTER_SANITIZE_SPECIAL_CHARS);
+        $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+    
+    
+
+ ?>
 
     <section>
         <h2>Dados</h2>
@@ -26,7 +42,11 @@
     <p><a href=""><link href="10-formulario-processamento.php">Voltar</a></p>
     
 
-    <!-- 2ª Digitação (aqui) -->
+<?php
+    }
+ } else {    
+
+?>
 
     
     <!-- Entra aqui porque ao entrar na página ele entra no falso e cai aqui (Mostra formulário) -->
@@ -45,7 +65,9 @@
         <button type="reset" name="limpar">Limpar</button>
     </form>
 
-    <!-- 3ª Digitação (aqui) -->
+ <?php
+    }
+ ?>
     
 </body>
 </html>
